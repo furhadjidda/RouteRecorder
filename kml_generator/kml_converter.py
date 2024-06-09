4d00cb69ed89d7bd464685c4f1bb11a99531df50
Furhad <furhadjidda@gmail.com>
2024-06-08 22:02:17 -0500

    Update KML script and reference data

--- kml_generator/kml_converter.py
+++ kml_generator/kml_converter.py
@@ -2,7 +2,7 @@ import folium
 import pandas as pd
 
 # Load the GPS data from a CSV file
-data = pd.read_csv('GPS_DATA.CSV')
+data = pd.read_csv('csv_data/GPSDATA.CSV')
 
 # Create a map centered at the first coordinate
 map_center = [data['latitude'].iloc[0], data['longitude'].iloc[0]]
@@ -13,11 +13,10 @@ for i in range(len(data)):
     lat = data['latitude'].iloc[i]
     lon = data['longitude'].iloc[i]
     timestamp = data['timestamp'].iloc[i]
-    altitude = data['altitude'].iloc[i]
     folium.Marker(
         location=[lat, lon],
-        popup=f"Time: {timestamp}\nAltitude: {altitude}m",
-        icon=folium.Icon(color='blue', icon='info-sign')
+        popup=f"Time: {timestamp}m",
+        icon=folium.Icon(color='darkpurple', icon='info-sign')
     ).add_to(mymap)
 
 # Save the map to an HTML file
